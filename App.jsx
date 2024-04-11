88a725ef5ed47b90434d823a4c146f5af482795e
Moraj <109418239+morajdhwaj@users.noreply.github.com>
2024-04-11 18:52:14 +0530

    [ADD] tailwind css

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import tw from "twrnc";
+
+const App = () => {
+  return (
+    <View>
+      <Text style={tw`bg-white h-full text-black`}>App</Text>
+    </View>
+  );
+};
+
+export default App;
