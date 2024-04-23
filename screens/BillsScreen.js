533b351814858e1f97cce3dc86eba6d50c51bef5
Moraj <109418239+morajdhwaj@users.noreply.github.com>
2024-04-23 13:39:14 +0530

    [UPDATE] Bills screen

--- screens/BillsScreen.js
+++ screens/BillsScreen.js
@@ -1,4 +1,11 @@
-import { View, Text, Image, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import tw from "twrnc";
 import axios from "axios";
@@ -28,6 +35,34 @@ const BillsScreen = () => {
       });
   };
 
+  const deleteBill = (id) => {
+    const options = {
+      method: "DELETE",
+      url: "https://billbox.catax.me/image/delete-image",
+      params: { image_id: id },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        getAllBills();
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  const createAlert = (id) =>
+    Alert.alert("Delete  Bill", "Are you want ton delete this bill", [
+      { text: "Yes", onPress: () => deleteBill(id) },
+      {
+        text: "No",
+        onPress: () => console.log("Cancel Pressed"),
+        style: "cancel",
+      },
+    ]);
+
   if (bills.length == 0) {
     return <Text style={tw`text-black`}>Loading...</Text>;
   }
@@ -35,17 +70,23 @@ const BillsScreen = () => {
   console.log(bills, "bills");
 
   return (
-    <ScrollView style={tw`text-black w-full`}>
-      <View style={tw`  mt-10 mx-5`}>
-        {bills.map((bill) => {
+    <ScrollView style={tw`bg-[#121212] h-full`}>
+      <View style={tw`  mx-5`}>
+        {bills?.reverse().map((bill) => {
           return (
             <View style={tw`  mt-10 mx-5`} key={bill.id}>
               <Text style={tw`text-black`}>{bill.id}</Text>
 
               <Image
                 source={{ uri: bill?.url }}
-                style={{ width: 300, height: 200 }}
+                style={{ width: 300, height: 200, borderRadius: 10 }}
               />
+              <TouchableOpacity
+                onPress={() => createAlert(bill?.id)}
+                style={tw`my-5 bg-[#9DE9D7]   self-start p-2 rounded`}
+              >
+                <Text style={tw`text-black`}>Delete</Text>
+              </TouchableOpacity>
             </View>
           );
         })}
